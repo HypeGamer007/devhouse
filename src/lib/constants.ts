f0d2f92096655bcd06886beebe3079ff93c065ee
hypegamer <dmill@hypegamer.gg>
2024-03-13 15:07:30 -0800

    prod push

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -44,7 +44,7 @@ export const pricingCards = [
     duration: 'month',
     highlight: 'Key features',
     features: ['Rebilling', '24/7 Support team'],
-    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+    priceId: 'price_1OqepTIad6m3wlrK1rrWPrtW',
   },
   {
     title: 'Basic',
@@ -53,7 +53,7 @@ export const pricingCards = [
     duration: 'month',
     highlight: 'Everything in Starter, plus',
     features: ['Unlimited Sub accounts', 'Unlimited Team members'],
-    priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    priceId: 'price_1OqepTIad6m3wlrK8rmHE0U2',
   },
 ]
 
