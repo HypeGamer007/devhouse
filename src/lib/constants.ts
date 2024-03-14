4b2a5ef3edcbd77603bca28689e3f39c13ed3922
hypegamer <dmill@hypegamer.gg>
2024-03-14 15:14:29 -0800

    add on product adjustment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -58,7 +58,7 @@ export const pricingCards = [
 ]
 
 export const addOnProducts = [
-  { title: 'Priority Support', id: 'prod_PNjJAE2EpP16pn' },
+  { title: 'Priority Support', id: 'prod_PjrBwMlE4HyiUC' },
 ]
 
 export const icons = [
